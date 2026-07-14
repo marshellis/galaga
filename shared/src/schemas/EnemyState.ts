46ddad13fd5a7e7c135257ec2369bc1834b31475
Ellis Jackson <ejackson@Mac.localdomain>
2026-07-14 16:19:26 -0600

    feat: add entering and angle fields to EnemyState schema

--- shared/src/schemas/EnemyState.ts
+++ shared/src/schemas/EnemyState.ts
@@ -1,11 +1,13 @@
 import { Schema, type } from "@colyseus/schema";
 
 export class EnemyState extends Schema {
-  @type("string") id: string = "";
-  @type("number") x: number = 0;
-  @type("number") y: number = 0;
-  @type("string") type: string = "basic";
-  @type("number") hp: number = 1;
-  @type("boolean") alive: boolean = true;
+  @type("string")  id: string      = "";
+  @type("number")  x: number       = 0;
+  @type("number")  y: number       = 0;
+  @type("string")  type: string    = "basic";
+  @type("number")  hp: number      = 1;
+  @type("boolean") alive: boolean  = true;
   @type("boolean") diving: boolean = false;
+  @type("boolean") entering: boolean = false;
+  @type("number")  angle: number   = 0;
 }
